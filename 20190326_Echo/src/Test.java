73de03fdb9b88c1e976827f2e4947308e9c2141e
SSyeon <tjdusdlfkrhd@naver.com>
2019-03-26 19:46:48 +0900

    add Echo Project

--- 20190326_Echo/src/Test.java
+++ 20190326_Echo/src/Test.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+		System.out.println("\n무슨 말이 하고싶으세요?>>>>>>>>");
+		String inputString = sc.nextLine();
+		
+		
+		if(inputString.equals("exit")) {
+			System.out.print("끝");
+			break;
+		 }
+		System.out.println("\n"+inputString);
 	}
 
+}
 }
