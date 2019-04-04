8af9ae88b03688771e2c65fe374a39594ef24fb9
SSyeon <tjdusdlfkrhd@naver.com>
2019-04-04 19:53:09 +0900

    add Nsum

---
+++ 20190404_Nsum/src/kr/hs/emirim/Nsum.java
@@ -0,0 +1,22 @@
+package kr.hs.emirim;
+
+import java.util.Scanner;
+
+public class Nsum {
+
+	public static void main(String[] args) {
+		System.out.println("숫자를 입력해주세요>>");
+		Scanner sc= new Scanner (System.in);
+		String num = sc.nextLine();
+//		int number[]=new int [num.length()];
+		int length=num.length();
+		int sum=0;
+		for(int i=0;i<length;i++) {				
+ 			//sum+=(int)(num.subString(i,i+1));
+			char n = num.charAt(i);
+			sum+= n -'0';
+ 		}
+ 			 System.out.println(sum);
+	}
+
+}
